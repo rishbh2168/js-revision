307c5e7f0a72e3ff0e4784537fafe53fdbb68fcc
rishbh2168 <98745068+rishbh2168@users.noreply.github.com>
2025-06-02 19:55:39 +0000

    comparision

---
+++ basic/comparision.js
@@ -0,0 +1,22 @@
+
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1) =  true convert krdega but try krna when compare same data type ho;
+// console.log("02" > 1);
+
+console.log(null > 0); //false
+console.log(null == 0);// false
+console.log(null >= 0);//true // in javascript it convert null to 0 that's why in 12 line it give false and in 14 line fuve true
+
+console.log(undefined == 0); // false
+console.log(undefined > 0); // false
+console.log(undefined < 0); // false
+
+// === 
+
+console.log("2" === 2); //false triple equal (===) check data type
