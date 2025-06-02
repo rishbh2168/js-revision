bf4723dbbad178f22c80dbe10c15acb3c3108452
rishbh2168 <98745068+rishbh2168@users.noreply.github.com>
2025-06-02 19:34:32 +0000

    operation and conversion

--- basic/conversion.js
+++ basic/conversion.js
@@ -42,7 +42,7 @@ let negValue = -value
 // console.log(2+2);
 // console.log(2-2);
 // console.log(2*2);
-// console.log(2**3);
+// console.log(2**3) power;
 // console.log(2/3);
 // console.log(2%3);
 
